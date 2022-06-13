77dd91f3cada72068746d9a8f623d5b12a5d6339
Heming233 <heming7159@163.com>
2022-06-13 11:02:47 +0800

    Initial commit

---
+++ src/main/java/com/folderrenameservice/entity/fileName.java
@@ -0,0 +1,12 @@
+package com.folderrenameservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class fileName {
+    private String name;
+}
