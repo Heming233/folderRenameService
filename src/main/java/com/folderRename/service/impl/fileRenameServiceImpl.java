cd0a12bb3ac11fcfa4fdbdd3aa0948c61d0236c8
Heming233 <heming7159@163.com>
2022-06-14 22:24:10 +0800

    fileRenameServiceImpl.java README.md

--- src/main/java/com/folderRename/service/impl/fileRenameServiceImpl.java
+++ src/main/java/com/folderRename/service/impl/fileRenameServiceImpl.java
@@ -1,9 +1,14 @@
 package com.folderRename.service.impl;
 
 import com.folderRename.mapper.fileMapper;
+import com.folderRename.service.fileRenameService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Heming233
@@ -11,8 +16,31 @@ import javax.annotation.Resource;
  */
 
 @Service
-public class fileRenameServiceImpl {
+public class fileRenameServiceImpl implements fileRenameService {
 
     @Resource
     fileMapper fileMapper;
+
+    @Override
+    public String renameOperation(List<File> fileList , List<String> nameList){
+        try{
+            Iterator<String> iter = nameList.iterator();
+            String filePath=null;
+            boolean result=true;
+            for(File file : fileList){
+                filePath = file.getAbsolutePath();
+                result=file.renameTo(new File(filePath+iter.next()));
+            }
+            if(result){
+                return "200";
+            }
+            else{
+                return "400";
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return "400";
+        }
+    }
 }
