a38f1fc0e8e016a2cea471e5071a8d6ec6c6421e
Heming233 <heming7159@163.com>
2022-06-18 10:02:57 +0800

    DemoApplication.java pom.xml

--- src/main/java/com/folderRename/demo/DemoApplication.java
+++ src/main/java/com/folderRename/demo/DemoApplication.java
@@ -2,13 +2,15 @@ package com.folderRename.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
 /**
  * @author Heming233
  * @version v1.0
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
